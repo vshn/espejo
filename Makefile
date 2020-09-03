2e6d4119ee6238aabd41e0cf4d789bde960ba58a
Chris <github.account@chrigel.net>
2020-09-03 15:08:31 +0200

    Add first integration tests

--- Makefile
+++ Makefile
@@ -23,11 +23,17 @@ else
 GOBIN=$(shell go env GOBIN)
 endif
 
+# Set Shell to bash, otherwise some targets fail with dash/zsh etc.
+SHELL := /bin/bash
+
 all: manager
 
-# Run tests
-test: generate fmt vet manifests
-	go test ./... -coverprofile cover.out
+# Run tests (see https://sdk.operatorframework.io/docs/building-operators/golang/references/envtest-setup)
+ENVTEST_ASSETS_DIR=$(shell pwd)/testbin
+integration_test: generate fmt vet manifests
+	mkdir -p ${ENVTEST_ASSETS_DIR}
+	test -f ${ENVTEST_ASSETS_DIR}/setup-envtest.sh || curl -sSLo ${ENVTEST_ASSETS_DIR}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/master/hack/setup-envtest.sh
+	source ${ENVTEST_ASSETS_DIR}/setup-envtest.sh; fetch_envtest_tools $(ENVTEST_ASSETS_DIR); setup_envtest_env $(ENVTEST_ASSETS_DIR); go test ./... -coverprofile cover.out -ginkgo.v
 
 .PHONY: dist
 dist:
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -6,18 +6,24 @@ http://www.apache.org/licenses/LICENSE-2.0
 package controllers
 
 import (
-	"path/filepath"
-	"testing"
-
+	"context"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"testing"
 
 	syncv1alpha1 "github.com/vshn/espejo/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
@@ -26,9 +32,12 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg                  *rest.Config
+	k8sClient            client.Client
+	testEnv              *envtest.Environment
+	syncConfigReconciler *SyncConfigReconciler
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -60,6 +69,9 @@ var _ = BeforeSuite(func(done Done) {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(k8sClient).ToNot(BeNil())
 
+	By("setting up the controller")
+	syncConfigReconciler = &SyncConfigReconciler{Client: k8sClient, Log: logf.Log, Scheme: scheme.Scheme}
+
 	close(done)
 }, 60)
 
@@ -68,3 +80,85 @@ var _ = AfterSuite(func() {
 	err := testEnv.Stop()
 	Expect(err).ToNot(HaveOccurred())
 })
+
+var _ = Describe("SyncConfig controller", func() {
+
+	It("should create new resources from SyncConfig", func() {
+
+		By("setting up test resources")
+		ns := "create-resource"
+		sourceNs := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: ns}}
+		cm := &v1.ConfigMap{
+			TypeMeta: metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-configmap",
+			},
+			Data: map[string]string{"PROJECT_NAME": "${PROJECT_NAME}"},
+		}
+		sc := &syncv1alpha1.SyncConfig{
+			ObjectMeta: metav1.ObjectMeta{Name: "test-syncconfig", Namespace: ns},
+			Spec: syncv1alpha1.SyncConfigSpec{
+				SyncItems:         []unstructured.Unstructured{toUnstructured(cm)},
+				NamespaceSelector: &syncv1alpha1.NamespaceSelector{MatchNames: []string{ns}},
+			},
+		}
+		Expect(k8sClient.Create(context.Background(), sourceNs)).ToNot(HaveOccurred())
+		Expect(k8sClient.Create(context.Background(), sc)).ToNot(HaveOccurred())
+
+		By("reconciling sync config")
+		result, err := syncConfigReconciler.Reconcile(ctrl.Request{
+			NamespacedName: types.NamespacedName{Name: sc.Name, Namespace: sc.Namespace},
+		})
+		Expect(err).ToNot(HaveOccurred())
+		Expect(result).ToNot(BeNil())
+
+		By("verify that resource exist")
+		syncResult := &v1.ConfigMap{}
+		key := types.NamespacedName{Namespace: ns, Name: cm.Name}
+		Expect(k8sClient.Get(context.Background(), key, syncResult)).ToNot(HaveOccurred())
+		Expect(syncResult.Data["PROJECT_NAME"]).To(BeEquivalentTo(ns))
+	})
+
+	It("should delete existing resources from SyncConfig", func() {
+
+		By("setting up test resources")
+		ns := "delete-resource"
+		sourceNs := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: ns}}
+		cm := &v1.ConfigMap{
+			TypeMeta: metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-configmap",
+				Namespace: ns,
+			},
+		}
+		sc := &syncv1alpha1.SyncConfig{
+			ObjectMeta: metav1.ObjectMeta{Name: "test-syncconfig", Namespace: ns},
+			Spec: syncv1alpha1.SyncConfigSpec{
+				DeleteItems:       []syncv1alpha1.DeleteMeta{{Name: cm.Name, Kind: cm.Kind, APIVersion: cm.APIVersion}},
+				NamespaceSelector: &syncv1alpha1.NamespaceSelector{MatchNames: []string{ns}},
+			},
+		}
+		Expect(k8sClient.Create(context.Background(), sourceNs)).ToNot(HaveOccurred())
+		Expect(k8sClient.Create(context.Background(), cm)).ToNot(HaveOccurred())
+		Expect(k8sClient.Create(context.Background(), sc)).ToNot(HaveOccurred())
+
+		By("reconciling sync config")
+		result, err := syncConfigReconciler.Reconcile(ctrl.Request{
+			NamespacedName: types.NamespacedName{Name: sc.Name, Namespace: sc.Namespace},
+		})
+		Expect(err).ToNot(HaveOccurred())
+		Expect(result).ToNot(BeNil())
+
+		By("verify that resource doesn't exist anymore")
+		err = k8sClient.Get(context.Background(), types.NamespacedName{Namespace: ns, Name: cm.Name}, &v1.ConfigMap{})
+		Expect(err).To(HaveOccurred())
+	})
+})
+
+func toUnstructured(configMap *v1.ConfigMap) unstructured.Unstructured {
+	obj := unstructured.Unstructured{}
+	m, err := runtime.DefaultUnstructuredConverter.ToUnstructured(configMap)
+	Expect(err).ToNot(HaveOccurred())
+	obj.SetUnstructuredContent(m)
+	return obj
+}

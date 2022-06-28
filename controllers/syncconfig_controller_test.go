9e98e118fc399e92c8e22b84113b54d4995d3233
Fabian Fischer <glrf@users.noreply.github.com>
2022-06-28 16:00:46 +0200

    Add test for sync update

--- controllers/syncconfig_controller_test.go
+++ controllers/syncconfig_controller_test.go
@@ -66,6 +66,41 @@ func (ts *SyncConfigControllerTestSuite) Test_GivenNewSyncConfig_WhenReconcile_T
 	ts.Assert().Equal(int64(1), sc.Status.SynchronizedItemCount)
 }
 
+func (ts *SyncConfigControllerTestSuite) Test_GivenSyncConfig_WhenReconcile_ThenUpdateResources() {
+	cm := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-configmap",
+		},
+		Data: map[string]string{"PROJECT_NAME": "${PROJECT_NAME}"},
+	}
+	sc := &SyncConfig{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-syncconfig", Namespace: ts.NS},
+		Spec: SyncConfigSpec{
+			SyncItems:         []unstructured.Unstructured{toUnstructured(ts.T(), cm)},
+			NamespaceSelector: &NamespaceSelector{MatchNames: []string{ts.NS}},
+		},
+	}
+	cm.Namespace = ts.NS
+  cm.Data["PROJECT_NAME"] = "wrong"
+  cm.Data["other"] = "new"
+	ts.EnsureResources(cm, sc)
+	result, err := ts.reconciler.Reconcile(context.TODO(), ctrl.Request{
+		NamespacedName: ts.MapToNamespacedName(sc),
+	})
+	ts.Require().NoError(err)
+	ts.Assert().NotNil(result)
+
+	ts.FetchResource(ts.MapToNamespacedName(cm), cm)
+	ts.Assert().Equal(ts.NS, cm.Data["PROJECT_NAME"])
+  ts.Assert().NotContains(cm.Data, "other")
+
+	ts.FetchResource(ts.MapToNamespacedName(sc), sc)
+	ts.Assert().Equal(int64(0), sc.Status.DeletedItemCount)
+	ts.Assert().Equal(int64(0), sc.Status.FailedItemCount)
+	ts.Assert().Equal(int64(1), sc.Status.SynchronizedItemCount)
+}
+
 func (ts *SyncConfigControllerTestSuite) Test_GivenSyncConfigWithDelete_WhenReconcile_ThenDeleteResource() {
 	cm := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
